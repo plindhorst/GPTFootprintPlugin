2094ddfd4195b339134977568605c430a30321ae
Simon <simon@biennier.fr>
2025-03-20 00:33:55 +0100

    use interval of 2s instead of dom change

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,20 +1,24 @@
-chrome.runtime.onMessage.addListener(async (request) => {
-  const window = await chrome.windows.getCurrent();
+chrome.runtime.onMessage.addListener((request) => {
+  const handler = async () => {
+    const window = await chrome.windows.getCurrent();
 
-  const height = 600;
-  const width = 400;
+    const height = 600;
+    const width = 400;
 
-  if (request == "openExtension") {
-    chrome.windows.create({
-      focused: true,
-      height,
-      left: window.width - width,
-      top: 0,
-      type: "popup",
-      url: "src/pages/popup/index.html",
-      width
-    }, () => {
-      console.log("Popup opened.");
-    });
-  }
+    if (request == "openExtension") {
+      chrome.windows.create({
+        focused: true,
+        height,
+        left: (window.width ?? 1920) - width,
+        top: 0,
+        type: "popup",
+        url: "src/pages/popup/index.html",
+        width
+      }, () => {
+        console.log("Popup opened.");
+      });
+    }
+  };
+
+  handler().catch(console.error);
 });
